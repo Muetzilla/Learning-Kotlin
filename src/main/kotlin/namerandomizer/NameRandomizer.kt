d3937061293f4af3a5c450c63430dc777d772165
Muetzilla <muetz-github@gasbichler.ch>
2021-10-28 17:57:12 +0200

    Add them sorted

--- src/main/kotlin/namerandomizer/NameRandomizer.kt
+++ src/main/kotlin/namerandomizer/NameRandomizer.kt
@@ -9,24 +9,24 @@ fun main(args: Array<String>) {
     val lastNames = getNamesFromFile("src\\main\\kotlin\\namerandomizer\\files\\lastNames.txt")
     val randomNames = ArrayList<String>()
 
-   for(i in 1..10){
-       randomNames.add(firstNames[Random.nextInt(firstNames.size)] + " " + lastNames[Random.nextInt(lastNames.size)])
-   }
-
-    randomNames.forEach{println(it)}
+    for (i in 1..10) {
+        randomNames.add(firstNames[Random.nextInt(firstNames.size)] + " " + lastNames[Random.nextInt(lastNames.size)])
+    }
+    randomNames.sort()
+    randomNames.forEach { println(it) }
     var namesAsOneString = ""
-    randomNames.forEach{namesAsOneString += it + "\n"}
+    randomNames.forEach { namesAsOneString += it + "\n" }
     writeNewNamesToFile("src\\main\\kotlin\\namerandomizer\\files\\newNames.txt", namesAsOneString)
 }
 
 
-    fun getNamesFromFile(filePath: String): MutableList<String> {
-        val inputStream: InputStream = File(filePath).inputStream()
-        val firstNames = mutableListOf<String>()
-        inputStream.bufferedReader().forEachLine { firstNames.add(it) }
-        return firstNames
-    }
+fun getNamesFromFile(filePath: String): MutableList<String> {
+    val inputStream: InputStream = File(filePath).inputStream()
+    val firstNames = mutableListOf<String>()
+    inputStream.bufferedReader().forEachLine { firstNames.add(it) }
+    return firstNames
+}
 
-fun writeNewNamesToFile(filePath:String, text:String){
+fun writeNewNamesToFile(filePath: String, text: String) {
     File(filePath).writeText(text)
 }
